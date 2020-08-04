00113cb79df736b9e524b28a9dca361ffae5c7ee
Leandro Moreno <123857@gmail.com>
2020-08-04 08:23:30 -0500

    Update 403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,1 +1,21 @@
-<p>hola
+@extends('layouts.app', ['activePage' => 'caracterizacion', 'titlePage' => __('Caracterización')])
+@section('title', __('Unauthorized'))
+@section('content')
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title ">{{ __('Sin permisos') }}</h4>
+              <p class="card-category"> {{ __('Su rol no le permite editar esta pagina.') }}</p>
+            </div>
+            <div class="card-body">
+              hola
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+@endsection
